4e6b0819fbe2b96eefa15a68c7801b69fb3cc124
sagacity <32454524+sagacity@users.noreply.github.com>
2018-02-26 09:03:17 +0800

    First

---
+++ Weather/pages/switchcity/switchcity.js
@@ -0,0 +1,181 @@
+// pages/switchcity/switchcity.js
+var expCom = require('../../utils/expressCompany.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    expressInfo: '', //快递信息
+    expressList: [],
+    expCom: '',
+    searchLetter: [],
+    scrollTop: 0,//置顶高度
+    scrollTopId: '',//置顶id
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+    const expressList = expCom.expressList();
+    const searchLetter = expCom.searchLetter;
+    const sysInfo = wx.getSystemInfoSync();
+    const winHeight = sysInfo.windowHeight;
+    const itemH = winHeight / searchLetter.length;
+    let tempArr = [];
+    searchLetter.map(
+      (item, index) => {
+        let temp = {};
+        temp.name = item;
+        temp.tHeight = index * itemH;
+        temp.bHeight = (index + 1) * itemH;
+        tempArr.push(temp)
+      }
+    );
+    this.setData({
+      expressList: expressList,
+      winHeight: winHeight,
+      itemH: itemH,
+      searchLetter: tempArr
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+  clickLetter: function (e) {
+    console.log(e.currentTarget.dataset.letter)
+    var showLetter = e.currentTarget.dataset.letter;
+    this.setData({
+      showLetter: showLetter,
+      isShowLetter: true,
+      scrollTopId: showLetter,
+    })
+    var that = this;
+    setTimeout(function () {
+      that.setData({
+        isShowLetter: false
+      })
+    }, 1000)
+  },
+  searchStart: function (e) {
+    var showLetter = e.currentTarget.dataset.letter;
+    var pageY = e.touches[0].pageY;
+    this.setScrollTop(this, showLetter);
+    this.nowLetter(pageY, this);
+    this.setData({
+      scrollTopId: showLetter,
+      isShowLetter: true,
+    })
+  },
+  searchMove: function (e) {
+    var pageY = e.touches[0].pageY;
+    var startPageY = this.data.startPageY;
+    var tHeight = this.data.tHeight;
+    var bHeight = this.data.bHeight;
+    var showLetter = 0;
+    console.log(pageY);
+    if (startPageY - pageY > 0) { //向上移动
+      if (pageY < tHeight) {
+        // showLetter=this.mateLetter(pageY,this);
+        this.nowLetter(pageY, this);
+      }
+    } else {//向下移动
+      if (pageY > bHeight) {
+        // showLetter=this.mateLetter(pageY,this);
+        this.nowLetter(pageY, this);
+      }
+    }
+  },
+  searchEnd: function (e) {
+    // console.log(e);
+    // var showLetter=e.currentTarget.dataset.letter;
+    var that = this;
+    setTimeout(function () {
+      that.setData({
+        isShowLetter: false
+      })
+    }, 1000)
+
+  },
+  nowLetter: function (pageY, that) {//当前选中的信息
+    var letterData = this.data.searchLetter;
+    var bHeight = 0;
+    var tHeight = 0;
+    var showLetter = "";
+    for (var i = 0; i < letterData.length; i++) {
+      if (letterData[i].tHeight <= pageY && pageY <= letterData[i].bHeight) {
+        bHeight = letterData[i].bHeight;
+        tHeight = letterData[i].tHeight;
+        showLetter = letterData[i].name;
+        break;
+      }
+    }
+
+    this.setScrollTop(that, showLetter);
+
+    that.setData({
+      bHeight: bHeight,
+      tHeight: tHeight,
+      showLetter: showLetter,
+      startPageY: pageY
+    })
+  },
+  bindScroll: function (e) {
+    console.log(e.detail)
+  },
+  setScrollTop: function (that, showLetter) {
+    that.setData({
+      scrollTopId: showLetter,
+    })
+  },
+  bindScroll: function (e) {
+    console.log(e.detail)
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
